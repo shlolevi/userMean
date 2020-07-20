a60c8d9de6dc6b8d55975d418534b05388ab4293
shlomil <shlomil@reali.com>
2020-07-21 01:39:23 +0300

    second

--- config/database.js
+++ config/database.js
@@ -1,4 +1,6 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/meanauth',
+    // database: 'mongodb://localhost:27017/meanauth',
+    // for heroku
+    database: 'mongodb+srv://meanauth:meanauth@meanauthapp.ldoph.mongodb.net/<dbname>?retryWrites=true&w=majority',
     secret: 'yoursecret'
 }
