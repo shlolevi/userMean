f1eb715520905edd130da813b7d4618d1337a663
shlomil <shlomil@reali.com>
2020-07-28 16:53:46 +0300

    commit all files

--- config/passport.js
+++ config/passport.js
@@ -28,22 +28,3 @@ module.exports = function(passport) {
     }));
 
 }
-
-// module.exports = function(passport){
-//     let opts = {};
-//     opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-//     opts.secretOrKey = config.secret;
-//     passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-//         User.findById(jwt_payload.data._id, (err, User) => {
-//             if(err){
-//                 return done(err, false);
-//             }
-//
-//             if(User){
-//                 return done(null, User);
-//             } else {
-//                 return done(null, false);
-//             }
-//         });
-//     }));
-// }
